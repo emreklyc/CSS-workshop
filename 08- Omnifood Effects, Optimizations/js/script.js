1c7b522dbb23f610bfba150f0fc8e1a29ad95155
emreklyc <messi61tr@gmail.com>
2023-09-16 15:19:17 +0300

    .

--- 08- Omnifood Effects, Optimizations/js/script.js
+++ 08- Omnifood Effects, Optimizations/js/script.js
@@ -51,6 +51,32 @@ allLinks.forEach(function (link) {
       headerEl.classList.toggle("nav-open");
   });
 });
+///////////////////////////////////////////////////////////
+
+// STICKY NAVIGATION
+
+const sectionHeroEl = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // in the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
 
 ///////////////////////////////////////////////////////////
 
